dc0b4aded45c5c2237468af7c7a04da234b128aa
jonathanx111 <jonathanx111@gmail.com>
2020-12-15 19:38:06 -0500

    commit tree 2b577802b09ebae1f533114c05311a8a149c8e75

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+// function findMatching(array, string) {
+//     return array.filter(function(e) {
+//         return e.toLowerCase() === string.toLowerCase()
+//     })
+// }
+
+findMatching = (array, string) => {
+    return array.filter(e => {
+        return e.toLowerCase() === string.toLowerCase()
+    })
+}
+
+function fuzzyMatch(drivers, string) {
+    return drivers.filter(e => {
+        return e.startsWith(string)
+    })
+}
+
+// fuzzyMatch = (drivers, string) => {
+//     return drivers.filter(e => {
+//         return e.startsWith(string)
+//     })
+// }
+
+// function matchName(drivers, string) {
+//     return drivers.filter(function (e) {
+//         return e.name === string
+//     })
+// }
+
+function matchName(drivers, string) {
+    return drivers.filter(e => e.name === string)
+}
+
